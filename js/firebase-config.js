a608f32aaaba7b7d604d7f04cf71ce2c3a09b7c6
NadaLemar <159011473+NadaLemar@users.noreply.github.com>
2026-09-08 10:19:10 +0700

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,21 +1,14 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+// ⚠️ 이 파일을 Firebase 콘솔에서 발급받은 값으로 채워주세요.
+// README.md의 "거래 게시판(Firebase) 설정하기" 섹션을 참고하세요.
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
+window.ETER_FIREBASE_CONFIG = {
   apiKey: "AIzaSyC_Mn61pVpkaNsv4UG0rx5ZBo_sRZvPne4",
   authDomain: "eternal-3feba.firebaseapp.com",
   projectId: "eternal-3feba",
   storageBucket: "eternal-3feba.firebasestorage.app",
   messagingSenderId: "332978389442",
-  appId: "1:332978389442:web:a80658fa4d8fc1bb16165b",
-  measurementId: "G-PZTN77B0SG"
+  appId: "1:332978389442:web:a80658fa4d8fc1bb16165b"
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+// 설정이 완료되었는지 자동으로 판별하기 위한 플래그입니다. 수정하지 마세요.
+window.ETER_FIREBASE_CONFIGURED = window.ETER_FIREBASE_CONFIG.apiKey !== "YOUR_API_KEY";
